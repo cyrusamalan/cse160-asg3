b5de258d0a87d74839c683a3bfdf48fc287d9938
Cyrus Amalan <cyrusamalan@Cyruss-MacBook-Air.local>
2025-02-16 20:29:32 -0800

    adding

--- src/Cube.js
+++ src/Cube.js
@@ -15,7 +15,6 @@ class Cube{
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);        
         
     
-/
     drawTriangle3DUV([0,0,0,    1,1,0,    1,0,0 ], [0,0,  1,1,  1,0]);
     drawTriangle3DUV([0,0,0,    0,1,0,    1,1,0 ], [0,0,  0,1,  1,1]);
 
