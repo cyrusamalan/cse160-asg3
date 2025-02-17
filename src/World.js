09a7c1ed7f04d1ba1b142572c19ef1baa2ff3a0e
Cyrus Amalan <cyrusamalan@Cyruss-MacBook-Air.local>
2025-02-16 19:59:17 -0800

    adding

--- src/World.js
+++ src/World.js
@@ -432,7 +432,8 @@ function renderAllShapes(){
   // Draw a yellow left arm
   var fan = new Cube();
   fan.color = [1,1,0,1];
-  fan.matrix.setTranslate(0,0,0.0);
+  fan.textureNum = -1
+  fan.matrix.setTranslate(0,-0.2,0.0);
   fan.matrix.rotate(-g_yellowAngle, 0, 0, 1); 
   fan.matrix.scale(0.25, 0.7, 0.5);
   fan.matrix.translate(-0.5, 0, 0);
@@ -440,7 +441,8 @@ function renderAllShapes(){
 
   var fan = new Cube();
   fan.color = [1,1,0,1];
-  fan.matrix.setTranslate(1,0.5,0.0);
+  fan.textureNum = -1
+  fan.matrix.setTranslate(1,0.2,0.0);
   fan.matrix.rotate(-g_yellowAngle, 0, 0, 1); 
   fan.matrix.scale(0.25, 0.7, 0.5);
   fan.matrix.translate(-0.5, 0, 0);
@@ -448,7 +450,8 @@ function renderAllShapes(){
 
   var fan = new Cube();
   fan.color = [1,1,0,1];
-  fan.matrix.setTranslate(-1,0.5,0.0);
+  fan.textureNum = -1
+  fan.matrix.setTranslate(-1,0.2,0.0);
   fan.matrix.rotate(-g_yellowAngle, 0, 0, 1); 
   fan.matrix.scale(0.25, 0.7, 0.5);
   fan.matrix.translate(-0.5, 0, 0);
